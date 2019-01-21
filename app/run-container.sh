02b99cafd42a16fcb6005e634e60f0eed2ace87e
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-01-21 23:47:16 +0100

    correct base_url in config and supervisord invocations

--- app/run-container.sh
+++ app/run-container.sh
@@ -9,26 +9,29 @@ SUPERVISORCTL="/usr/bin/supervisorctl"
 SUPERVISORD="/usr/bin/supervisord"
 SUEXEC="/sbin/su-exec"
 
+SUPERVISORCTLOPTS="-u dummy -p dummy"
+SUPERVISORDOPTS="-c /etc/supervisord.conf"
+
 $SUEXEC pompa bundle exec rake db:migrate
 
 reload() {
   SIDEKIQ_PID="`cat tmp/pids/sidekiq.pid 2> /dev/null`"
   [ "x$SIDEKIQ_PID" != "$SIDEKIQ_PID" ] && kill -TSTP $SIDEKIQ_PID
 
-  $SUPERVISORCTL reload
+  $SUPERVISORCTL $SUPERVISORCTLOPTS reload
 }
 
 shutdown() {
   SIDEKIQ_PID="`cat tmp/pids/sidekiq.pid 2> /dev/null`"
   [ "x$SIDEKIQ_PID" != "$SIDEKIQ_PID" ] && kill -TSTP $SIDEKIQ_PID
 
-  $SUPERVISORCTL shutdown
+  $SUPERVISORCTL $SUPERVISORCTLOPTS shutdown
 }
 
 trap reload 1
 trap shutdown 2 15
 
-$SUPERVISORD -c /etc/supervisord.conf
+$SUPERVISORD $SUPERVISORDOPTS
 
 while (kill -0 `cat /var/run/supervisord.pid 2> /dev/null` > /dev/null 2>&1) ; do
   sleep 5
