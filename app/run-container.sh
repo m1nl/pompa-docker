8ac291ab143c36cd166d449a7539217c2f60e9b4
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-05-18 21:21:21 +0200

    correct run-container.sh

--- app/run-container.sh
+++ app/run-container.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
   
-set -eu
-
 ln -s /proc/$$/fd/1 /dev/docker-stdout
 ln -s /proc/$$/fd/2 /dev/docker-stderr
 
@@ -12,17 +10,17 @@ SUEXEC="/sbin/su-exec"
 SUPERVISORCTLOPTS="-u dummy -p dummy"
 SUPERVISORDOPTS="-c /etc/supervisord.conf"
 
-$SUEXEC pompa bundle exec rake db:migrate
+$SUEXEC pompa bundle exec rake db:migrate || exit 1
 
 reload() {
-  SIDEKIQ_PID="`cat tmp/pids/sidekiq.pid 2> /dev/null`"
+  SIDEKIQ_PID="`$SUPERVISORCTL $SUPERVISORCTLOPTS pid sidekiq 2> /dev/null`"
   [ "x$SIDEKIQ_PID" != "$SIDEKIQ_PID" ] && kill -TSTP $SIDEKIQ_PID
 
   $SUPERVISORCTL $SUPERVISORCTLOPTS reload
 }
 
 shutdown() {
-  SIDEKIQ_PID="`cat tmp/pids/sidekiq.pid 2> /dev/null`"
+  SIDEKIQ_PID="`$SUPERVISORCTL $SUPERVISORCTLOPTS pid sidekiq 2> /dev/null`"
   [ "x$SIDEKIQ_PID" != "$SIDEKIQ_PID" ] && kill -TSTP $SIDEKIQ_PID
 
   $SUPERVISORCTL $SUPERVISORCTLOPTS shutdown
@@ -31,7 +29,7 @@ shutdown() {
 trap reload 1
 trap shutdown 2 15
 
-$SUPERVISORD $SUPERVISORDOPTS
+$SUPERVISORD $SUPERVISORDOPTS || exit 1
 
 while (kill -0 `cat /var/run/supervisord.pid 2> /dev/null` > /dev/null 2>&1) ; do
   sleep 5
